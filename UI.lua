4a584ca4ec0591c699adf9b4ff82b7f220f64ec6
EndeyshentLabs <Themikfound@gmail.com>
2023-05-07 19:40:56 +0300

    Add status text. stylua.

--- UI.lua
+++ UI.lua
@@ -8,38 +8,38 @@ UIElement = {}
 ---@param h number Height
 ---@return table
 function UIElement:new(x, y, w, h)
-    local private = {}
+	local private = {}
 
-    local public = {}
-    public.x = x
-    public.y = y
-    public.w = w
-    public.h = h
+	local public = {}
+	public.x = x
+	public.y = y
+	public.w = w
+	public.h = h
 
-    function UIElement:draw()
-        love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-    end
+	function UIElement:draw()
+		love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+	end
 
-    function public:isHovered()
-        local mX = love.mouse.getX()
-        local mY = love.mouse.getY()
+	function public:isHovered()
+		local mX = love.mouse.getX()
+		local mY = love.mouse.getY()
 
-        if mX >= self.x and mX <= self.x + self.w and mY >= self.y and mY <= self.y + self.h then
-            return true
-        end
-        return false
-    end
+		if mX >= self.x and mX <= self.x + self.w and mY >= self.y and mY <= self.y + self.h then
+			return true
+		end
+		return false
+	end
 
-    function public:isClicked()
-        if self:isHovered() and love.mouse.isDown(1) then
-            return true
-        end
-        return false
-    end
+	function public:isClicked()
+		if self:isHovered() and love.mouse.isDown(1) then
+			return true
+		end
+		return false
+	end
 
-    setmetatable(public, self)
-    self.__index = self
-    return public
+	setmetatable(public, self)
+	self.__index = self
+	return public
 end
 
 ---@class ColorButton
@@ -57,75 +57,75 @@ ColorButton = extended(UIElement)
 ---@param textColor string Hex string of the label text color
 ---@return table
 function ColorButton:new(text, fun, x, y, w, h, normalColor, hoveredColor, textColor)
-    local private = {}
+	local private = {}
 
-    local public = {}
-    public.text = text
-    public.x = x
-    public.y = y
-    public.w = w
-    public.h = h
+	local public = {}
+	public.text = text
+	public.x = x
+	public.y = y
+	public.w = w
+	public.h = h
 
-    function ColorButton:draw()
-        if self:isHovered() then
-            setColorHEX(hoveredColor) -- #ffffff
-        else
-            setColorHEX(normalColor) -- #282828
-        end
-        love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-        setColorHEX(textColor) -- #ff0000
-        love.graphics.print(
-            self.text,
-            Font,
-            self.x + (self.w / 2) - Font:getWidth(self.text) / 2,
-            self.y + (self.h / 2) - Font:getHeight() / 2
-        )
-    end
+	function ColorButton:draw()
+		if self:isHovered() then
+			setColorHEX(hoveredColor) -- #ffffff
+		else
+			setColorHEX(normalColor) -- #282828
+		end
+		love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+		setColorHEX(textColor) -- #ff0000
+		love.graphics.print(
+			self.text,
+			Font,
+			self.x + (self.w / 2) - Font:getWidth(self.text) / 2,
+			self.y + (self.h / 2) - Font:getHeight() / 2
+		)
+	end
 
-    function public:isHovered()
-        local mX = love.mouse.getX()
-        local mY = love.mouse.getY()
+	function public:isHovered()
+		local mX = love.mouse.getX()
+		local mY = love.mouse.getY()
 
-        if mX >= self.x and mX <= self.x + self.w and mY >= self.y and mY <= self.y + self.h then
-            return true
-        end
-        return false
-    end
+		if mX >= self.x and mX <= self.x + self.w and mY >= self.y and mY <= self.y + self.h then
+			return true
+		end
+		return false
+	end
 
-    function public:isClicked()
-        if self:isHovered() and love.mouse.isDown(1) then
-            return true
-        end
-        return false
-    end
+	function public:isClicked()
+		if self:isHovered() and love.mouse.isDown(1) then
+			return true
+		end
+		return false
+	end
 
-    function ColorButton:update(dt)
-        if self:isClicked() then
-            fun()
-        end
-    end
+	function ColorButton:update(dt)
+		if self:isClicked() then
+			fun()
+		end
+	end
 
-    setmetatable(public, self)
-    self.__index = self
-    return public
+	setmetatable(public, self)
+	self.__index = self
+	return public
 end
 
 Button = extended(ColorButton)
 
 function Button:draw()
-    if self:isHovered() then
-        setColorHEX("#ffffff")
-    else
-        setColorHEX("#282828")
-    end
-    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-    setColorHEX("#ff0000")
-    love.graphics.print(
-        self.text,
-        Font,
-        self.x + (self.w / 2) - Font:getWidth(self.text) / 2,
-        self.y + (self.h / 2) - Font:getHeight() / 2
-    )
+	if self:isHovered() then
+		setColorHEX("#ffffff")
+	else
+		setColorHEX("#282828")
+	end
+	love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+	setColorHEX("#ff0000")
+	love.graphics.print(
+		self.text,
+		Font,
+		self.x + (self.w / 2) - Font:getWidth(self.text) / 2,
+		self.y + (self.h / 2) - Font:getHeight() / 2
+	)
 end
 
 ---@class ColorCheckButton
@@ -140,66 +140,66 @@ ColorCheckButton = extended(UIElement)
 ---@param disabledColor string Hex string of the color when button is UNckecked
 ---@return table
 function ColorCheckButton:new(x, y, w, h, enabledColor, disabledColor)
-    local private = {}
+	local private = {}
 
-    local public = {}
-    public.x = x
-    public.y = y
-    public.w = w
-    public.h = h
-    public.enabled = false
+	local public = {}
+	public.x = x
+	public.y = y
+	public.w = w
+	public.h = h
+	public.enabled = false
 
-    function ColorCheckButton:draw()
-        if self.enabled then
-            setColorHEX(enabledColor)
-        else
-            setColorHEX(disabledColor)
-        end
-        love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-    end
+	function ColorCheckButton:draw()
+		if self.enabled then
+			setColorHEX(enabledColor)
+		else
+			setColorHEX(disabledColor)
+		end
+		love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+	end
 
-    function public:isHovered()
-        local mX = love.mouse.getX()
-        local mY = love.mouse.getY()
+	function public:isHovered()
+		local mX = love.mouse.getX()
+		local mY = love.mouse.getY()
 
-        if mX >= self.x and mX <= self.x + self.w and mY >= self.y and mY <= self.y + self.h then
-            return true
-        end
-        return false
-    end
+		if mX >= self.x and mX <= self.x + self.w and mY >= self.y and mY <= self.y + self.h then
+			return true
+		end
+		return false
+	end
 
-    function public:isClicked()
-        local once = false
-        if self:isHovered() and love.mouse.isDown(1) and not once then
-            once = true
-            return true
-        end
-        return false
-    end
+	function public:isClicked()
+		local once = false
+		if self:isHovered() and love.mouse.isDown(1) and not once then
+			once = true
+			return true
+		end
+		return false
+	end
 
-    function ColorCheckButton:update(dt)
-        if self:isClicked() then
-            self.enabled = not self.enabled
-        end
-    end
+	function ColorCheckButton:update(dt)
+		if self:isClicked() then
+			self.enabled = not self.enabled
+		end
+	end
 
-    function ColorCheckButton:isEnabled()
-        return self.enabled
-    end
+	function ColorCheckButton:isEnabled()
+		return self.enabled
+	end
 
-    setmetatable(public, self)
-    self.__index = self
-    return public
+	setmetatable(public, self)
+	self.__index = self
+	return public
 end
 
 ---@class CheckButton
 CheckButton = extended(ColorCheckButton)
 
 function CheckButton:draw()
-    if self.enabled then
-        setColorHEX("#00ff00")
-    else
-        setColorHEX("#ff0000")
-    end
-    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+	if self.enabled then
+		setColorHEX("#00ff00")
+	else
+		setColorHEX("#ff0000")
+	end
+	love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
 end
--- main.lua
+++ main.lua
@@ -1,60 +1,77 @@
 Font = nil
 List = {}
 Cursor = 0
+StatusText = "Welcome to LoDo. Made by EndeyshentLabs"
 
 require("utils")
 require("UI")
 
 function love.load()
-    Font = love.graphics.newFont("res/fonts/Lato-Light.ttf", 24)
+	Font = love.graphics.newFont("res/fonts/Lato-Light.ttf", 24)
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor(0.04, 0.04, 0.04)
-    setColorHEX("#ffffff")
-    love.graphics.print(tostring(Cursor), Font, 300, 300)
+	love.graphics.setBackgroundColor(0.04, 0.04, 0.04)
+	setColorHEX("#ffffff")
+	love.graphics.print(tostring(Cursor), Font, 300, 300)
 
-    if List then
-        for k, v in pairs(List) do
-            if v.done then
-                v.x = 200
-            else
-                v.x = 0
-            end
-            v:draw()
-        end
-    end
+	if List then
+		for k, v in pairs(List) do
+			if v.done then
+				v.x = 200
+			else
+				v.x = 0
+			end
+			v:draw()
+		end
+	end
+
+	setColorHEX("#202020")
+	love.graphics.rectangle(
+		"fill",
+		0,
+		love.graphics.getHeight() - 26,
+		love.graphics.getWidth(),
+		love.graphics.getHeight()
+	)
+	local mode = "T"
+	if List[Cursor] and List[Cursor].done then
+		mode = "D"
+	else
+		mode = "T"
+	end
+	setColorHEX("#efefef")
+	love.graphics.print((" %s | %s"):format(mode, StatusText), Font, 0, love.graphics.getHeight() - 28)
 end
 
 function love.update(dt)
-    if Cursor < 0 and #List == 0 then
-        Cursor = 0
-    end
-    if Cursor < 1 and #List > 0 then
-        Cursor = 1
-    end
-    if Cursor > #List then
-        Cursor = #List
-    end
+	if Cursor < 0 and #List == 0 then
+		Cursor = 0
+	end
+	if Cursor < 1 and #List > 0 then
+		Cursor = 1
+	end
+	if Cursor > #List then
+		Cursor = #List
+	end
 end
 
 function love.keypressed(key)
-    if love.keyboard.isDown("i") then
-        List[#List + 1] = Button:new("TODO", function()
-        end, 0, #List * 102, 100, 100)
-        List[#List].done = false
-        Cursor = #List
-    end
-    if love.keyboard.isDown("down") then
-        Cursor = Cursor + 1
-    end
-    if love.keyboard.isDown("up") then
-        Cursor = Cursor - 1
-    end
-    if love.keyboard.isDown("space") then
-        if Cursor > 0 then List[Cursor].done = not List[Cursor].done end
-    end
-    -- if love.keyboard.isDown("d") then
-    --     List[Cursor] = nil
-    -- end
+	if love.keyboard.isDown("i") then
+		List[#List + 1] = Button:new("TODO", function()
+		end, 0, #List * 102, 100, 100)
+		List[#List].done = false
+		Cursor = #List
+	end
+	if love.keyboard.isDown("down") then
+		Cursor = Cursor + 1
+	end
+	if love.keyboard.isDown("up") then
+		Cursor = Cursor - 1
+	end
+	if love.keyboard.isDown("space") then
+		if Cursor > 0 then
+			List[Cursor].done = not List[Cursor].done
+		end
+	end
 end
